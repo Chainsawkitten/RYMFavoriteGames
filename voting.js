1264a91d0b80a1ba24b9d064c8ce2bca3296bf8d
Albin Bernhardsson <catscratchesp@gmail.com>
2016-04-12 20:06:08 +0200

    displayResults and base getVotes
    Methods to display results and get votes from file contents.

--- voting.js
+++ voting.js
@@ -1,4 +1,27 @@
 /**
+ * Display the results on the web page.
+ * Arguments:
+ *  games - The list of games to display.
+ */
+function displayResults(games) {
+    var div = document.getElementById('results');
+    div.innerHTML = "";
+    
+    for (var i = 0; i < games.length; ++i) {
+        div.innerHTML += '<div class="game">' + games[i].name + '</div>\n';
+    }
+}
+
+/**
+ * Get votes from text file.
+ * Arguments:
+ *  fileContents - Contents of the vote file.
+ */
+function getVotes(fileContents) {
+    return [];
+}
+
+/**
  * main()
  */
 window.onload = main;
@@ -10,7 +33,7 @@ function main() {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if (request.readyState == 4) {
-            document.getElementById('results').innerHTML = request.responseText;
+            displayResults(getVotes(request.responseText));
         }
     };
     request.open("GET", "votes.txt", true);
